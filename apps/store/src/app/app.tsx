fb029d5c6d89115a79b4b60e085945d2ca5a9836
Carlos Flach <carlos.flach@dynamox.net>
2023-01-17 17:54:52 -0300

    feat: setting up fake api, also added app structure.

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -1,13 +1,35 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
+import {getAllGames } from '../fake-api';
+
+import { Card, CardActionArea, CardContent, CardMedia, Typography } from '@material-ui/core';
 
 export function App() {
   return (
-    <>
-      <NxWelcome title="store" />
-      <div />
-    </>
+    <div className="container">
+      <div className='games-layout'>
+        {getAllGames().map((x) => (
+          <Card key={x.id} className="game-card">
+            <CardActionArea>
+              <CardMedia className='game-card-media'
+              image={x.image} title={x.name} />
+              <CardContent>
+                <Typography gutterBottom variant='h5' component='h2'>
+                  {x.name}
+                </Typography>
+                <Typography variant='body2' color="textSecondary" component='p' >
+                  {x.description}
+                </Typography>
+                <Typography variant='body2' color='textSecondary' component='p'
+                className='game-rating'>
+                  <strong>Rating: </strong> {x.rating}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        ))}
+      </div>
+    </div>
   );
 }
 
