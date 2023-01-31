c856348fa7ca270c1c95d4a8a6fd44854f03a76f
Carlos Flach <carlos.flach@dynamox.net>
2023-01-31 19:17:12 -0300

    feat: implement api request at route based on id.

--- libs/store/feature-game-detail/src/lib/store-feature-game-detail.tsx
+++ libs/store/feature-game-detail/src/lib/store-feature-game-detail.tsx
@@ -1,5 +1,13 @@
-import {Card, CardActionArea, CardContent, Typography} from '@material-ui/core'
-import { useParams } from 'react-router-dom'
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from '@material-ui/core';
+import { formatRating } from '@nxegghead/store/util-formatters';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 // export interface IProps {
 //     match: {
@@ -11,21 +19,67 @@ import { useParams } from 'react-router-dom'
 
 // type IStoreFeatureGameDetails = (props: IProps) => JSX.Element;
 
-
 export const StoreFeatureGameDetail = () => {
-
   const params = useParams();
+  const [state, setState] = useState<{
+    data: any;
+    loadingState: 'success' | 'error' | 'loading';
+  }>({
+    data: {},
+    loadingState: 'success',
+  });
+
+  useEffect(() => {
+    setState({
+      ...state,
+      loadingState: 'loading',
+    });
+
+    const gameId = params.id;
+    fetch(`api/games/${gameId}`)
+      .then((x) => x.json())
+      .then((res) => {
+        setState({
+          ...state,
+          data: res,
+          loadingState: 'success',
+        });
+      })
+      .catch((err) => {
+        setState({
+          ...state,
+          loadingState: 'error',
+        });
+      });
+  }, [params.id]);
+
   return (
-    <div className='container'>
-    <Card>
-      <CardActionArea>
-        <CardContent>
-          <Typography variant="body2" color="textSecondary" component='p'>
-            {params.id}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <div className="container">
+      {state.loadingState === 'loading' ? (
+        'Loading ...'
+      ) : state.loadingState === 'error' ? (
+        <div>Error fetching data</div>
+      ) : state.data === null ? (
+        ''
+      ) : (
+        <Card>
+          <CardActionArea>
+            <CardMedia
+              className='game-card-media'
+              image={state.data.image}
+              title={state.data.name}
+            />
+            <CardContent>
+              <Typography variant="body2" color="textSecondary" component="p">
+                {state.data.name}
+              </Typography>
+              <Typography variant='body2' color="textSecondary" component="p" className='game-rating'>
+                <strong>Rating:</strong> {formatRating(state.data.rating)}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      )}
     </div>
-  )
-}
+  );
+};
