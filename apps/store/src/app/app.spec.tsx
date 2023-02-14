2ea661fa7aed8eb04dbfc501fbf24d87a07a0ed3
Carlos Flach <carlos.flach@dynamox.net>
2023-02-14 08:25:46 -0300

    feat: run tests for a react app with Nx

--- apps/store/src/app/app.spec.tsx
+++ apps/store/src/app/app.spec.tsx
@@ -2,7 +2,20 @@ import { render } from '@testing-library/react';
 
 import App from './app';
 
+const mockedFetch = (data) => {
+  return jest.fn().mockImplementation(() => {
+    return Promise.resolve({
+      ok: true,
+      json: () => data,
+    });
+  });
+};
+
 describe('App', () => {
+  beforeEach(() => {
+    window.fetch = mockedFetch([])
+  })
+
   it('should render successfully', () => {
     const { baseElement } = render(<App />);
     expect(baseElement).toBeTruthy();
@@ -10,6 +23,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
-    expect(getByText(/Welcome store/gi)).toBeTruthy();
+    expect(getByText(/Board Game Hoard/gi)).toBeTruthy();
   });
 });
